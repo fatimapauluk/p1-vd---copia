9a42b0ca10d537215d53d32d5e915ab5ae25f0ad
Fatima Pauluk <fpauluk@mail.utdt.edu>
2024-09-16 23:51:38 -0300

    septimo commit

--- src/lib/Caras.svelte
+++ src/lib/Caras.svelte
@@ -156,7 +156,9 @@ let imagesdos = [
   <main>
     <div class="chart-container">
       <div class="chart-wrapper">
-        <iframe src='https://flo.uri.sh/visualisation/19289560/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:600px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/visualisation/19289560/?utm_source=embed&utm_campaign=visualisation/19289560' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>      
+        <iframe src='https://flo.uri.sh/visualisation/19289560/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:600px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'>
+        </iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/visualisation/19289560/?utm_source=embed&utm_campaign=visualisation/19289560' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a>
+        </div>      
       </div>
   
       <div class="chart-wrapper">
